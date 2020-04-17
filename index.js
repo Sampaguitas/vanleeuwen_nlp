f21b6cc4bdc8bce9e2fab967973da3ee9db03d31
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2020-04-17 17:50:00 +0400

    changes

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const glob = require('glob');
+const _ = require('lodash');
+const fs = require('fs');
+const app = express();
+
+//bodyParser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// app.post("/", (req, res) => {
+//     res.status(200).send('');
+// });
+
+app.post("/dialogflow", express.json(), (req, res) => {
+    const agent = new WebhookClient({ request: req, response: res });
+    let intentMap = new Map();
+    intentMap.set("Default Welcome Intent", welcome);
+    intentMap.set("Default Fallback Intent", defaultFallback);
+    agent.handleRequest(intentMap);
+});
+
+function welcome(agent) {
+    agent.add('Hi, I am assistant. I can help you in various service. How can I help you today?');
+}
+
+
+function defaultFallback(agent) {
+    agent.add('Sorry! I am unable to understand this at the moment. I am still learning humans. You can pick any of the service that might help me.');
+}
+
+module.exports = { welcome: welcome, defaultFallback: defaultFallback };
+
+
+// Listen on port
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on ${port}`));
+
+// Compile all routers   
+var routeFolders = [],     
+routePaths = "./routes"   
+glob.sync('**/*', { cwd: routePaths }).forEach(route => {     
+    var _isFolder = !_.endsWith(route, '.js')     
+    route = '/' + route.replace(/\.[^/.]+$/, '')     
+    if (!_.endsWith(route, 'index')) {       
+        var _router = require(routePaths + route)       
+        app.use(route, _router)       
+        if (_isFolder) routeFolders.push(route)     }   })   
+        routeFolders.forEach(route => {     var _pathDeindex = routePaths + route + '/deindex.js'     
+        if (fs.existsSync(_pathDeindex))       
+        app.use(route, require(_pathDeindex))   
+    })
