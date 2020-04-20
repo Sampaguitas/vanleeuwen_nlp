48dd24b3115fc32152de1b35ca54d25d25787cc6
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2020-04-20 22:51:29 +0400

    changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const _ = require('lodash');
 
-let sizes = require('./_constants/sizes');
+let dimensions = require('./_constants/dimensions');
 
 //bodyParser middleware
 app.use(bodyParser.urlencoded({extended:false}));
@@ -23,10 +23,13 @@ mongoose
 const { WebhookClient } = require("dialogflow-fulfillment");
 
 app.post("/dialogflow", express.json(), (req, res) => {
+    console.log('toto');
     const agent = new WebhookClient({ request: req, response: res });
     let intentMap = new Map();
     intentMap.set("Default Welcome Intent", welcome);
-    intentMap.set("getOd", getOd)
+    intentMap.set("Outside Diameter", getOutsideDiameter);
+    intentMap.set("Wall Thickness", getWallThickness);
+    intentMap.set("Weight", getWeight);
     agent.handleRequest(intentMap);
 });
 
@@ -34,22 +37,64 @@ function welcome(agent) {
     agent.add('Hi, How can I help you today?');
 }
 
-function getOd(agent) {
+function getOutsideDiameter(agent) {
     let sizeOne = _.isArray(agent.parameters.sizeOne) && !_.isEmpty(agent.parameters.sizeOne) ? agent.parameters.sizeOne[0] :  agent.parameters.sizeOne;
     let item = _.isArray(agent.parameters.item) && !_.isEmpty(agent.parameters.item) ? agent.parameters.item[0] :  agent.parameters.item;
-    if (item != 'pipe') {
-        agent.add(`Sorry, I havn't been trained to retrive the OD of ${items} yet`);
-    } else {
-        let found = sizes.find(element => element.tags.includes(sizeOne));
-        if (!_.isUndefined(found) && !!found.mm) {
-            agent.add(`the Outside Diameter of a ${sizeOne} pipe is ${found.mm} millimeters`);
-        }
+    switch (item) {
+        case 'pipe':
+            let found = dimensions.find(element => element.item === item && element.sizeOne.tags.includes(sizeOne));
+            if (!_.isUndefined(found) && found.dimensions.hasOwnProperty('outsideDiameter')) {
+                return agent.add(`the outside diameter of a ${sizeOne} ${item} is ${found.dimensions.outsideDiameter.metric.value} ${found.dimensions.outsideDiameter.metric.uom}`);
+            } else {
+                return agent.add(`Sorry. I could not find the outside diameter for this item`);
+            }
+        default: agent.add(`Sorry, I havn't been trained for ${item} yet`);
     }
 }
 
-module.exports = { welcome: welcome, getOd, getOd };
+function getWallThickness(agent) {
+    let sizeOne = _.isArray(agent.parameters.sizeOne) && !_.isEmpty(agent.parameters.sizeOne) ? agent.parameters.sizeOne[0] :  agent.parameters.sizeOne;
+    let scheduleOne = _.isArray(agent.parameters.scheduleOne) && !_.isEmpty(agent.parameters.scheduleOne) ? agent.parameters.scheduleOne[0] :  agent.parameters.scheduleOne;
+    let item = _.isArray(agent.parameters.item) && !_.isEmpty(agent.parameters.item) ? agent.parameters.item[0] :  agent.parameters.item;
+    switch (item) {
+        case 'pipe':
+            let found = dimensions.find(function (element) {
+                return element.item === item && element.sizeOne.tags.includes(sizeOne) && element.scheduleOne.tags.includes(scheduleOne);
+            });
+            if (!_.isUndefined(found) && found.dimensions.hasOwnProperty('wallThickness')) {
+                return agent.add(`the wall thickness of a ${sizeOne} ${item} ${scheduleOne} is ${found.dimensions.wallThickness.metric.value} ${found.dimensions.wallThickness.metric.uom}`);
+            } else {
+                return agent.add(`Sorry. I could not find the wall thickness for this item`);
+            }
+        default: agent.add(`Sorry, I havn't been trained for ${item} yet`);
+    }
+}
 
+function getWeight(agent) {
+    let sizeOne = _.isArray(agent.parameters.sizeOne) && !_.isEmpty(agent.parameters.sizeOne) ? agent.parameters.sizeOne[0] :  agent.parameters.sizeOne;
+    let scheduleOne = _.isArray(agent.parameters.scheduleOne) && !_.isEmpty(agent.parameters.scheduleOne) ? agent.parameters.scheduleOne[0] :  agent.parameters.scheduleOne;
+    let item = _.isArray(agent.parameters.item) && !_.isEmpty(agent.parameters.item) ? agent.parameters.item[0] :  agent.parameters.item;
+    switch (item) {
+        case 'pipe':
+            let found = dimensions.find(function (element) {
+                return element.item === item && element.sizeOne.tags.includes(sizeOne) && element.scheduleOne.tags.includes(scheduleOne);
+            });
+            if (!_.isUndefined(found) && found.dimensions.hasOwnProperty('weight')) {
+                return agent.add(`the weight of a ${sizeOne} ${item} ${scheduleOne} is ${found.dimensions.weight.metric.value} ${found.dimensions.weight.metric.uom}`);
+            } else {
+                return agent.add(`Sorry. I could not find the weight for this item`);
+            }
+        default: agent.add(`Sorry, I havn't been trained for ${item} yet`);
+    }
+}
+
+module.exports = { 
+    welcome: welcome,
+    getOutsideDiameter: getOutsideDiameter,
+    getWallThickness: getWallThickness,
+    getWeight: getWeight
+};
 
 // Listen on port
-const port = process.env.PORT || 8080// || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on ${port}`));
