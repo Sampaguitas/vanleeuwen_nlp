6bf09ca03b361a5a4ed2b79c6ec417b7a5225ddb
Timothee Desurmont <timothee.desurmont@sampaguitas.com>
2020-04-25 14:35:12 +0400

    changes

--- functions/getOutsideDiameter.js
+++ functions/getOutsideDiameter.js
@@ -6,8 +6,9 @@ function getOutsideDiameter(agent) {
     //console.log(agent.session);
     // let ctx = `${agent.session}/contexts/outside_diameter_dialog_params_item`
     let whatNext = constants.whatNextArray[Math.floor(Math.random() * constants.whatNextArray.length)];
-    let sizeOne = _.isArray(agent.parameters.sizeOne) && !_.isEmpty(agent.parameters.sizeOne) ? agent.parameters.sizeOne[0] :  agent.parameters.sizeOne;
     let item = _.isArray(agent.parameters.item) && !_.isEmpty(agent.parameters.item) ? agent.parameters.item[0] :  agent.parameters.item;
+    let sizeOne = _.isArray(agent.parameters.sizeOne) && !_.isEmpty(agent.parameters.sizeOne) ? agent.parameters.sizeOne[0] :  agent.parameters.sizeOne;
+    // let sizeTwo = _.isArray(agent.parameters.sizeTwo) && !_.isEmpty(agent.parameters.sizeTwo) ? agent.parameters.sizeTwo[0] :  agent.parameters.sizeTwo;
     // let angle = _.isArray(agent.parameters.angle) && !_.isEmpty(agent.parameters.angle) ? agent.parameters.angle[0] :  agent.parameters.angle;
     // let radius = _.isArray(agent.parameters.radius) && !_.isEmpty(agent.parameters.radius) ? agent.parameters.radius[0] :  agent.parameters.radius;
     switch (item) {
